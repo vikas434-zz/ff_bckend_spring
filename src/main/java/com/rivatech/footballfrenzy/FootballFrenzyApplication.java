7c5424e375db616c7595ca59123c24990b4d3009
Vikas Ranjan <vranjan@mobileiron.com>
2018-11-04 21:04:33 +0530

    other commit

---
+++ src/main/java/com/rivatech/footballfrenzy/FootballFrenzyApplication.java
@@ -0,0 +1,13 @@
+package com.rivatech.footballfrenzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFrenzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballFrenzyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rivatech/footballfrenzy/FootballFrenzyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rivatech.footballfrenzy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootballFrenzyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
